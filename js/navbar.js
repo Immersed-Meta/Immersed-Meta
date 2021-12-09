a7a1c6f0a4e25903af479383aaccc0cb3d2f0278
Jorgen10 <strabjorgen@gmail.com>
2021-12-09 22:31:12 +0100

    Navbar optimised
    removed like 8 lines of HTML and switched it out with a script tag and 2 lines of code in a js file

---
+++ js/navbar.js
@@ -0,0 +1,2 @@
+let navbar = String('<div class="menu"><img src="images/logo-50px.gif" class="icon"><a href="index.html" class="top bordered">Home</a><a href="about.html" class="top bordered">About</a><a href="community.html" class="top bordered">Community</a><a href="creations.html" class="top bordered">Creations</a><a href="credits.html" class="top bordered">Credits</a></div>');
+document.body.outerHTML = navbar + document.body.outerHTML;
